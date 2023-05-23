62e1dc2a82a02873a76d8d0915cd318f1ada7de3
Blaze Sanders <blaze.sanders@spoke-aiot.com>
2023-05-22 23:32:57 -0700

    Define parallel CPU main function with real time clock

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 from nicegui import ui
+from datetime import datetime
+
 
 darkMode = ui.dark_mode()
 darkMode.disable()
@@ -16,30 +18,27 @@ def toggle_dark_mode():
         isDarkModeOn = not isDarkModeOn 
         darkModeSwitch.style('color: #FF0000')
 
-darkModeSwitch = ui.switch('Enable Dark Mode', on_change=toggle_dark_mode).style('color: #FF0000')
-
-
-
-
-
 def findSystemName(text):
     if text.upper() == 'L':
         return "Lights" 
     else:
         return text
 
-with ui.grid(columns=2):
-    ui.icon('home', color='red').classes('text-10xl')
-    ui.icon('search', color='blue').classes('text-10xl')
+if __name__ in {"__main__", "__mp_main__"}:
+
+    with ui.grid(columns=2):
+        darkModeSwitch = ui.switch('Enable Dark Mode', on_change=toggle_dark_mode).style('color: #FF0000')
+        timerLabel = ui.label()
+
+        ui.timer(1.0, lambda: timerLabel.set_text(f'{datetime.now():%X}'))
+
+        ui.icon('home', color='red').classes('text-5xl')
+        ui.icon('search', color='blue').classes('text-5xl, w-100')
     
-    ui.input(label='System', placeholder='',
-         on_change=lambda e: result.set_text(findSystemName(e.value)),
-         validation={'Input too long': lambda value: len(value) < 20})
+        ui.input(label='System', placeholder='', \
+                 on_change=lambda e: result.set_text(findSystemName(e.value)), \
+                 validation={'Input too long': lambda value: len(value) < 20})
 
     result = ui.label()
 
-
-
-
-
-ui.run(native=True)
+    ui.run(native=True)
