53933fae76d9569b2063fd035b68ef9f90c1d374
Blaze Sanders <blazes@mfc.us>
2023-07-18 16:31:08 -0500

    Touchscreen switch part URLs and micropython code

---
+++ SwitchController.py
@@ -0,0 +1,11 @@
+
+# https://www.amazon.com/12V-24V-Single-Controller-Dimmer-Brightness/dp/B01MTQSIG7/ref=asc_df_B01MTQSIG7/?tag=hyprod-20&linkCode=df0&hvadid=241980973821&hvpos=&hvnetw=g&hvrand=15885852254165248059&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9011640&hvtargid=pla-567804767262&th=1
+# https://www.amazon.com/Wall-mounted-Dimmer-Switch-Brightness-Controller/dp/B07L3MT41G/ref=pd_bxgy_img_sccl_1/139-8951308-4939835?pd_rd_w=GUzMt&content-id=amzn1.sym.26a5c67f-1a30-486b-bb90-b523ad38d5a0&pf_rd_p=26a5c67f-1a30-486b-bb90-b523ad38d5a0&pf_rd_r=CWVB1K49ZAF1R45PEA5Z&pd_rd_wg=JEpGA&pd_rd_r=b19c3427-9237-4651-855b-c1339b7616b0&pd_rd_i=B07L3MT41G&th=1
+# https://www.amazon.com/stores/page/4CDE47BE-84B7-46F6-AB85-13CC6D868DA4?ingress=0&visitId=f1f15a16-84a6-4121-a61b-009c456dd507&ref_=as_li_ss_tl
+# https://www.tiktok.com/@levvenelectronics
+# https://iottysmarthome.com/pages/technical
+# https://moeshouse.com/products/us-zigbee-glass-touch-3-way-light-switch?variant=39797103231057&utm_source=google02&utm_medium=cpc&network=x&keyword=&campaignid=20331101469&gad=1&gclid=Cj0KCQjw8NilBhDOARIsAHzpbLCOGAFpJEkwFIaA0apU5OMHG9eeueIXlx7d8Sr8kw7ulh1h-UlmdzQaAn7QEALw_wcB
+
+
+
+class SwitchController:
